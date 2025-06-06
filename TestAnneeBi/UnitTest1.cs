5fe0c7f2204d8bb13f2a02a5445741fb7d259fa2
Nath <nathlej41@gmail.com>
2025-06-06 16:14:48 +0200

    [RED]

--- TestAnneeBi/UnitTest1.cs
+++ TestAnneeBi/UnitTest1.cs
@@ -13,6 +13,25 @@ namespace HelloWorldTests
             Assert.True(resultat);
         }
 
+        [Fact(DisplayName = "2019 pas bis")]
+        public void Annee_2019_N_Est_Pas_Bissextile()
+        {
+            bool resultat = Calendrier.EstBissextile(2019);
+            Assert.False(resultat);
+        }
+        [Fact(DisplayName ="1900 pas bis")]
+        public void Annee_1900_N_Est_Pas_Bissextile()
+        {
+            bool resultat = Calendrier.EstBissextile(1900);
+            Assert.False(resultat);
+        }
+        [Fact(DisplayName = "2000 est bis")]
+        public void Annee_2000_Est_Bissextile()
+        {
+            bool resultat = Calendrier.EstBissextile(2000);
+            Assert.True(resultat);
+        }
+
     }
 
 
